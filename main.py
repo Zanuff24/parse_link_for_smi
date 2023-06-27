c6178ac02261ca5fcc645771c770a9a36ec65ec8
Zanuff24 <Zanuff24@gmail.com>
2023-06-27 10:30:43 +0300

    добавлены команды start и help, которые сообщают какой формат ввода сообщений создан обработчик сообщения, который проверяет сколько слов в сообщении от пользователя. если 2, то считает как комментарий + ссылку

--- main.py
+++ main.py
@@ -4,77 +4,63 @@ import telebot
 from telebot import types
 import data
 import datetime
+import smi_list
 
 bot = telebot.TeleBot(data.token) #https://t.me/parse_smi_bot
+
+# dict. хранит запись вида id чата с сообщением, которое хранит последний отправленный текст от пользователя
+# текст должен содержать ссылку + комментарий к ссылке
 bd = {}
 
-def saveFile(link, chat, smi):
+#запись в файл
+# data дата отправки сообщения
+# smi название сми
+# link ссылка на пост
+# comment  комментарий к ссылке
+# chat id отправившего сообщение
+def saveFile(chat, text, smi):
     with open('link_list.csv', "a", encoding='cp1251', newline='') as fill:
         data_now = datetime.datetime.now()
-        names = ["data", "smi", "link", "chat"]
+        link, comment = text.split()
+        names = ["data", "smi", "link", " comment", "chat"]
         file_writer = csv.DictWriter(fill, delimiter=",",
                                      lineterminator="\r", fieldnames=names)
-        file_writer.writerow({"data": data_now, "smi": smi, "link": link, "chat": chat})
+        file_writer.writerow({"data": data_now, "smi": smi, "link": link, " comment": comment, "chat": chat})
 
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    bot.send_message(message.chat.id, "отправьте сообщение в формате: \n" +
+                     "<b>ссылка комментарий </b>\n" +
+                     "одна ссылка и комментарий за один раз", parse_mode='html')
+#создание клавиатуры. список кнопок берется из записей smi_l файла smi_list
 
 def keyboard():
     kb = types.InlineKeyboardMarkup()
-    sa = types.InlineKeyboardButton(text='Советская Адыгея', callback_data='sa')
-    am = types.InlineKeyboardButton(text='Адыгэ макъ', callback_data='am')
-    mn = types.InlineKeyboardButton(text='Майкопские новости', callback_data='mn')
-    aif = types.InlineKeyboardButton(text='АИФ Адыгея', callback_data='aif')
-    at = types.InlineKeyboardButton(text='Адыгея сегодня', callback_data='at')
-    gtrk = types.InlineKeyboardButton(text='ГТРК', callback_data='gtrk')
-    mtv = types.InlineKeyboardButton(text='МТВ', callback_data='mtv')
-    sogl = types.InlineKeyboardButton(text='Согласие', callback_data='sogl')
-    ed = types.InlineKeyboardButton(text='Единство', callback_data='ed')
-    tv = types.InlineKeyboardButton(text='Теучежские вести', callback_data='tv')
-    kz = types.InlineKeyboardButton(text='Красное знамя', callback_data='kz')
-    mayak = types.InlineKeyboardButton(text='Маяк', callback_data='mayak')
-    druz = types.InlineKeyboardButton(text='Дружба', callback_data='druz')
-    zara = types.InlineKeyboardButton(text='Заря', callback_data='zara')
-    kv = types.InlineKeyboardButton(text='Кошехабльские вести', callback_data='kv')
-    ttv = types.InlineKeyboardButton(text='Тахтамукайское ТВ', callback_data='ttv')
-    eic = types.InlineKeyboardButton(text='Единый информационный центр Шовгеновского района', callback_data='eic')
-
-    kb.row(sa)
-    kb.row(am)
-    kb.row(mn)
-    kb.row(aif)
-    kb.row(at)
-    kb.row(gtrk)
-    kb.row(mtv)
-    kb.row(sogl)
-    kb.row(ed)
-    kb.row(tv)
-    kb.row(kz)
-    kb.row(mayak)
-    kb.row(druz)
-    kb.row(zara)
-    kb.row(kv)
-    kb.row(ttv)
-    kb.row(eic)
+    for el, name in smi_list.smi_l.items():
+        kb.row(types.InlineKeyboardButton(text=name, callback_data=el))
     return kb
 
+#прием сообщения в формате "ссылка + текст комментария"
 @bot.message_handler(content_types=["text"])
 def any_mes(message):
-    bd[message.chat.id] = message.text
+    if len(message.text.split()) != 2:
+        bot.send_message(message.chat.id, "отправьте сообщение в формате: \n"+
+                         "<b>ссылка комментарий </b>\n" +
+                         "одна ссылка и комментарий за один раз", parse_mode='html')
+    else:
+        bd[message.chat.id] = message.text
 
-    key = keyboard()
-    bot.send_message(message.chat.id, "сми", reply_markup=key)
+        key = keyboard()
+        bot.send_message(message.chat.id, "сми", reply_markup=key)
 
-    print(message.chat.id, message.text)
-
-    print(bd[message.chat.id])
+        print(message.chat.id, message.text)
+        print(bd[message.chat.id])
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    # Если сообщение из чата с ботом
-
-    match call.data:
-        case "sa":
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="сми")
-            bot.send_message(call.message.chat.id, text=f'link{call.message.chat.id} + {bd[call.message.chat.id]}')
-            saveFile(call.message.chat.id, bd[call.message.chat.id], 'Советская Адыгея')
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="сми")
+    bot.send_message(call.message.chat.id, text=f'link{call.message.chat.id} + {bd[call.message.chat.id]}')
+    saveFile(call.message.chat.id, bd[call.message.chat.id], smi_list.smi_l[call.data])
 
 bot.polling(none_stop=True)

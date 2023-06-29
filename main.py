f4fd7372a758c14dba3e64cc9627d419cb39e078
Zanuff24 <Zanuff24@gmail.com>
2023-06-29 11:14:38 +0300

    отработка крашей меню добавлен отработчик комментария теперь коммент из нескльких слов

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ bd = {}
 def saveFile(chat, text, smi):
     with open('link_list.csv', "a", encoding='cp1251', newline='') as fill:
         data_now = datetime.datetime.now()
-        link, comment = text.split()
+        first_whitespace = text.find(' ')
+        link = text[:first_whitespace]
+        comment = text[first_whitespace:]
+        #link, comment = text.split()
         names = ["data", "smi", "link", " comment", "chat"]
         file_writer = csv.DictWriter(fill, delimiter=",",
                                      lineterminator="\r", fieldnames=names)
@@ -48,23 +51,25 @@ def keyboard():
 #прием сообщения в формате "ссылка + текст комментария"
 @bot.message_handler(content_types=["text"])
 def any_mes(message):
-    if len(message.text.split()) != 2:
+    if len(message.text.split()) < 2:
         bot.send_message(message.chat.id, "отправьте сообщение в формате: \n" +
                          "<b>ссылка комментарий </b>\n" +
-                         "одна ссылка и комментарий за один раз", parse_mode='html')
+                        "пример \n" +
+                        "https://aif.ru/ день независимости \n"
+                         "одна ссылка и комментарий за один раз через пробел", parse_mode='html')
     else:
         bd[message.chat.id] = message.text
 
         key = keyboard()
         bot.send_message(message.chat.id, "сми", reply_markup=key)
 
-        print(message.chat.id, message.text)
-        print(bd[message.chat.id])
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="сми")
-    bot.send_message(call.message.chat.id, text=f'link{call.message.chat.id} + {bd[call.message.chat.id]}')
-    saveFile(call.message.chat.id, bd[call.message.chat.id], smi_list.smi_l[call.data])
-
+    try:
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="сми")
+        bot.send_message(call.message.chat.id, text=f'данные сохранены')
+        saveFile(call.message.chat.id, bd[call.message.chat.id], smi_list.smi_l[call.data])
+    except:
+        bot.send_message(call.message.chat.id, text=f'нет данных для сохранения. введите данные для отправки еще раз')
 bot.polling(none_stop=True)
